0c17462f0fe64a2eaafb8b0dab84a0113684e8c5
Fansy <418536538@qq.com>
2021-08-24 23:22:57 +0800

    feat: add paths resolve

---
+++ src/paths.ts
@@ -0,0 +1,41 @@
+import { isString, concat } from 'lodash-es';
+import glob from 'fast-glob';
+import { ParserType } from './interfaces';
+
+const extensions: Record<ParserType, string[]> = {
+    less: ['less'],
+};
+
+/**
+ * Normalize window and unix path
+ * @param path path to normalize
+ */
+const normalize = (path: string) => path.replace(/\\/g, '/');
+
+/**
+ * Filtering invalid path of `ParserType`
+ * @param paths paths to filter
+ * @param type parser type
+ * @returns
+ * New array of paths
+ */
+const filter = (paths: string[], type: ParserType) => {
+    return concat(
+        [],
+        ...extensions[type].map(extension => {
+            const pattern = new RegExp(`.*\\.${extension}$`);
+
+            return paths.filter(path => path.match(pattern));
+        })
+    );
+};
+
+export const resolve = async (paths: string | string[], type: ParserType) => {
+    const _paths = isString(paths) ? [paths] : paths;
+
+    const normalizedPaths = _paths.map(normalize);
+
+    const resolvedPaths = await glob(normalizedPaths, { onlyFiles: true });
+
+    return filter(resolvedPaths, type);
+};
